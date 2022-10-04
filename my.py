19fed2df663df23b2ab57b969d88e57a43646a74
malloy <mowork2021@163.com>
2022-10-04 13:44:02 +0800

    commit: 添加蚁群聚类算法

--- my.py
+++ my.py
@@ -7,23 +7,28 @@ if __name__ == '__main__':
     x = iris.data[:, 0]  # X- Axis -sepal length
     y = iris.data[:, 1]  # Y- Axis - sepal length
     species = iris.target  # Species
-    print(species)
     x_min, x_max = x.min() - 0.5, x.max() + 0.5
     y_min, y_max = y.min() - 0.5, x.max() + 0.5
 
-    # Scatterplot
-    plt.figure()
-    plt.title("Iris Dataset - Classification By Sepal Sizes")
-    plt.scatter(x, y, c=species)
-    plt.xlabel('Sepal length')
-    plt.ylabel('Sepal width')
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
+    # # Scatterplot
+    # plt.figure()
+    # plt.title("Iris Dataset - Classification By Sepal Sizes")
+    # plt.scatter(x, y, c=species)
+    # plt.xlabel('Sepal length')
+    # plt.ylabel('Sepal width')
+    # plt.xlim(x_min, x_max)
+    # plt.ylim(y_min, y_max)
+    # plt.xticks(())
+    # plt.yticks(())
+    # plt.show()
 
     predict = clusteringNew.run()
+    i = 0
+    count = 0
+    for i in range(len(predict)):
+        if(species[i] == predict[i]):
+            count+=1
+    print((float)(count)/(float)(len(predict)))
     plt.figure()
     plt.title("predict res")
     plt.scatter(x, y, c=predict)
