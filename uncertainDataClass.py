2e5909e079a586a3e5034cd3d2fb46a66ecdff3b
malloy <mowork2021@163.com>
2022-12-10 11:47:27 +0800

    commit: 不确定数据集分类

---
+++ uncertainDataClass.py
@@ -0,0 +1,34 @@
+import math
+import operator
+import random
+import time
+
+import matplotlib.pyplot as plt
+import numpy
+import numpy as np
+import sklearn.datasets as ds
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import pandas as pd
+from AntClassfied import Ant
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('iris_data.csv')
+    X = data.drop(['target', 'label'], axis=1)
+    y = data.loc[:, 'label']
+    # 预处理，将数据规范化
+    X_norm = StandardScaler().fit_transform(X)
+    pca = PCA(n_components=3)
+    X_pca = pca.fit_transform(X_norm)
+    ablity = np.array([[0] for _ in range(150)])
+    for i in range(150):
+        if random.random() < 0.8:
+            ablity[i][0] = 1
+        else :
+            ablity[i][0] = 0
+
+    sample = np.hstack((X_pca, ablity))
+    print(ablity)
+    print(sample)
+    # Ant()
